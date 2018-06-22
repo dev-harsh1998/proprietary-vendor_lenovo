58075bc5259359312213db3af9e734e91a3cbf72
dev-harsh1998 <harshit.india@linaro.org>
2018-06-22 17:06:20 +0530

    a6000: Update media blobs to LA.BR.1.2.9.1

--- a6000/a6000-vendor.mk
+++ a6000/a6000-vendor.mk
@@ -199,7 +199,19 @@ PRODUCT_COPY_FILES += \
     vendor/lenovo/a6000/proprietary/vendor/etc/init/android.hardware.drm@1.0-service.widevine.rc:system/vendor/etc/init/android.hardware.drm@1.0-service.widevine.rc \
     vendor/lenovo/a6000/proprietary/vendor/lib/soundfx/libqcbassboost.so:system/vendor/lib/soundfx/libqcbassboost.so \
     vendor/lenovo/a6000/proprietary/vendor/lib/soundfx/libqcreverb.so:system/vendor/lib/soundfx/libqcreverb.so \
-    vendor/lenovo/a6000/proprietary/vendor/lib/soundfx/libqcvirt.so:system/vendor/lib/soundfx/libqcvirt.so
+    vendor/lenovo/a6000/proprietary/vendor/lib/soundfx/libqcvirt.so:system/vendor/lib/soundfx/libqcvirt.so \
+    vendor/lenovo/a6000/proprietary/vendor/lib/libOmxAlacDec.so:system/vendor/lib/libOmxAlacDec.so \
+    vendor/lenovo/a6000/proprietary/vendor/lib/libOmxAlacDecSw.so:system/vendor/lib/libOmxAlacDecSw.so \
+    vendor/lenovo/a6000/proprietary/vendor/lib/libOmxAmrwbplusDec.so:system/vendor/lib/libOmxAmrwbplusDec.so \
+    vendor/lenovo/a6000/proprietary/vendor/lib/libOmxApeDec.so:system/vendor/lib/libOmxApeDec.so \
+    vendor/lenovo/a6000/proprietary/vendor/lib/libOmxApeDecSw.so:system/vendor/lib/libOmxApeDecSw.so \
+    vendor/lenovo/a6000/proprietary/vendor/lib/libOmxWmaDec.so:system/vendor/lib/libOmxWmaDec.so \
+    vendor/lenovo/a6000/proprietary/lib/libFileMux.so:system/lib/libFileMux.so \
+    vendor/lenovo/a6000/proprietary/lib/libOmxMux.so:system/lib/libOmxMux.so \
+    vendor/lenovo/a6000/proprietary/lib/libOmxVpp.so:system/lib/libOmxVpp.so \
+    vendor/lenovo/a6000/proprietary/lib/libmmosal.so:system/lib/libmmosal.so \
+    vendor/lenovo/a6000/proprietary/lib/libmmparser_lite.so:system/lib/libmmparser_lite.so \
+    vendor/lenovo/a6000/proprietary/lib/libvpplibrary.so:system/lib/libvpplibrary.so
 
 PRODUCT_PACKAGES += \
     libloc_api_v02 \
